c027bb8e22b9f97c812882d61785e54fe92b19db
JeanHaro <JeanCHL98@hotmail.com>
2026-06-26 23:12:54 -0500

    59 - Navbar - Navegar entre paginas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Componentes
+import { Navbar } from './shared/components/navbar/navbar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Navbar,
+    RouterOutlet
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
