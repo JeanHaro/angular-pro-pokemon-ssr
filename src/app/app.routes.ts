4a2582bcb9ab5b04f5f2ba3f3f9f6e1705678bb3
JeanHaro <JeanCHL98@hotmail.com>
2026-06-29 20:58:53 -0500

    63 - Construccion y ejecucion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  },
+  {
     path: 'about',
     loadComponent: () => import('./pages/about-page/about-page')
   },
