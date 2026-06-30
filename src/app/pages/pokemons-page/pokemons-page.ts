378768e47f67ed0f52ee13ae0a09b4340111fe9c
JeanHaro <JeanCHL98@hotmail.com>
2026-06-30 00:22:54 -0500

    71 - Skeleton Loader

--- src/app/pages/pokemons-page/pokemons-page.ts
+++ src/app/pages/pokemons-page/pokemons-page.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 // Componentes
 import { PokemonList } from "../../pokemons/components/pokemon-list/pokemon-list";
+import { PokemonListSkeleton } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton";
 
 @Component({
   selector: 'pokemons-page',
   imports: [
-    PokemonList
-  ],
+    PokemonList,
+    PokemonListSkeleton
+],
   templateUrl: './pokemons-page.html',
   styles: ``,
 })
-export default class PokemonsPage {}
+export default class PokemonsPage implements OnInit {
+  public isLoading = signal(true);
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 5000);
+  }
+}
