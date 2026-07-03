60bc9b5b2f468e9fd9072f73e589b70debda2c15
JeanHaro <JeanCHL98@hotmail.com>
2026-07-03 15:11:29 -0500

    probando soluciones para el prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -8,24 +8,14 @@ export const serverRoutes: ServerRoute[] = [
     path: 'pokemons/page/:page',
     renderMode: RenderMode.Prerender,
     async getPrerenderParams() {
-      return Array.from(
-        { length: TOTAL_PAGES },
-        (_, i) => ({
-          page: `${i + 1}`,
-        })
-      );
+      return Array.from({ length: TOTAL_PAGES }, (_, i) => ({ page: `${i + 1}` }));
     },
   },
   {
     path: 'pokemons/:id',
     renderMode: RenderMode.Prerender,
     async getPrerenderParams() {
-      return Array.from(
-        { length: TOTAL_POKEMONS },
-        (_, i) => ({
-          id: `${i + 1}`,
-        })
-      );
+      return Array.from({ length: TOTAL_POKEMONS }, (_, i) => ({ id: `${i + 1}` }));
     },
   },
   {
