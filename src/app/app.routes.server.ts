79dde7f52717f92feb17dd8a7df573e36569bf44
JeanHaro <JeanCHL98@hotmail.com>
2026-07-03 15:18:08 -0500

    fix: prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,23 +1,6 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-const TOTAL_POKEMONS = 151;
-const TOTAL_PAGES = 5;
-
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: 'pokemons/page/:page',
-    renderMode: RenderMode.Prerender,
-    async getPrerenderParams() {
-      return Array.from({ length: TOTAL_PAGES }, (_, i) => ({ page: `${i + 1}` }));
-    },
-  },
-  {
-    path: 'pokemons/:id',
-    renderMode: RenderMode.Prerender,
-    async getPrerenderParams() {
-      return Array.from({ length: TOTAL_POKEMONS }, (_, i) => ({ id: `${i + 1}` }));
-    },
-  },
   {
     path: '**',
     renderMode: RenderMode.Prerender,
