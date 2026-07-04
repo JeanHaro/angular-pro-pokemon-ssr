98b8b2669f393a3325b7f6f4daf97cf284a906c0
JeanHaro <JeanCHL98@hotmail.com>
2026-07-03 21:19:55 -0500

    99 - Probar carga perezosa y navegacion

--- src/app/app.routes.spec.ts
+++ src/app/app.routes.spec.ts
@@ -1,30 +1,50 @@
 import { Location } from '@angular/common';
 import { routes } from './app.routes';
-import { Router } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
+import { TestBed } from '@angular/core/testing';
+
+// Componente
+import AboutPage from './pages/about-page/about-page';
 
 describe('App Routes', () => {
   let router: Router;
   let location: Location;
 
   beforeEach(() => {
-    // todo:
+    TestBed.configureTestingModule({
+      providers: [provideRouter(routes)]
+    });
+
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
   });
 
   it('should be defined', () => {
     expect(routes).toBeDefined();
-    expect(routes.length).toBe(6);
   });
 
   it('should contain all defined routes', () => {
-    // todo:
+    expect(routes.length).toBe(6);
   });
 
   it('should render AboutPageComponent when path is /about', async () => {
-    // todo:
+    const aboutRoute = routes.find( (route) => route.path === 'about' )!;
+
+    // Verificamos que exista la ruta del about
+    expect(aboutRoute).toBeDefined();
+
+    // Cargamos el componente
+    const component = (await aboutRoute.loadComponent!()) as any;
+
+    // Verificamos que el componente que renderizo es el about
+    expect(component.default).toBe(AboutPage);
   });
 
-  it('should navigate to "/about" and render AboutPageComponent', async () => {
-    // todo:
+  // Verificamos que al colocar o navegar al slash navegamos al about
+  it('should navigate to "/about" when default path is set', async () => {
+    await router.navigate(['/']);
+
+    expect(location.path()).toBe('/about');
   });
 
   it('should render PricingPageComponent when path is /pricing', async () => {
