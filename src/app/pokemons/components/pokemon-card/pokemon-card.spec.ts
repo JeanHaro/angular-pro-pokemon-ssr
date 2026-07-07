a762c6fe6a9e5e4efb20fb436cb2226a8c1aba77
JeanHaro <JeanCHL98@hotmail.com>
2026-07-06 23:07:34 -0500

    102 - Probar atributos y directivas

--- src/app/pokemons/components/pokemon-card/pokemon-card.spec.ts
+++ src/app/pokemons/components/pokemon-card/pokemon-card.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { provideRouter } from "@angular/router";
+import { provideRouter, RouterLink } from "@angular/router";
+import { By } from "@angular/platform-browser";
 
 // Componentes
 import { PokemonCard } from "./pokemon-card";
 
 // Interfaces
 import { SimplePokemon } from "../../interfaces/simple-pokemon.interface";
+
 
 const mockPokemon: SimplePokemon = {
   id: '1',
@@ -35,13 +37,40 @@ describe('PokemonCard', () => {
     expect(component).toBeTruthy();
   });
 
+  // Verificamos que recibe el input
   it('should have the SimplePokemon signal input', () => {
     expect(component.pokemon()).toBe(mockPokemon);
   });
 
+  // Verificamos que tenga la misma url
   it('should compute the correct pokemon image URL', () => {
     const expectedUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mockPokemon.id}.png`
 
     expect(component.pokemonImage()).toBe(expectedUrl);
-  })
+  });
+
+  // Verificamos que el nombre y la imagen rendericen correctamente
+  it('should render pokemon name and image correctly', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const nameElement = compiled.querySelector('h2');
+    const imgElement = compiled.querySelector('img');
+
+    expect(nameElement?.textContent.trim()).toBe(mockPokemon.name);
+    expect(imgElement?.src).toBe(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mockPokemon.id}.png`);
+    expect(imgElement?.alt).toBe(mockPokemon.name);
+  });
+
+  // Debería tener correctamente el routeLink
+  it('should have the correct routeLink configuration', () => {
+    const debugElement = fixture.debugElement.query(
+      By.directive(RouterLink)
+    );
+
+    const routerLinkInstance = debugElement.injector.get(RouterLink);
+
+    const expectedUrl = `/pokemons/${mockPokemon.name}`;
+
+    expect(routerLinkInstance.urlTree?.toString()).toBe(expectedUrl);
+  });
 });
